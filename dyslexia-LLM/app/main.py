c1e3a714f4facac22d5673d3e0bb9e2f74550961
Oliver Jones <oliver10jones220@gmail.com>
2026-07-26 21:26:31 +0100

    slackened api policy

--- dyslexia-LLM/app/main.py
+++ dyslexia-LLM/app/main.py
@@ -10,10 +10,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://dyslexia-chatbot-gfa7.onrender.com",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
