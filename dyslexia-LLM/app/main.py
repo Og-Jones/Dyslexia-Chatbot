a4a859571eeeab209fd5e2d1ef072ff9dd4dbcd9
Oliver Jones <oliver10jones220@gmail.com>
2026-07-26 21:22:54 +0100

    changed origin policy

--- dyslexia-LLM/app/main.py
+++ dyslexia-LLM/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Dyslexia Help API",
     description=("A RAG-based API for answering questions about dyslexia using trusted sources."),
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://dyslexia-chatbot-gfa7.onrender.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
