2902ce1abca250b000f340c2c09a3417dda59415
Oliver Jones <oliver10jones220@gmail.com>
2026-09-07 17:03:34 +0100

    generate.py contains the code calling the openAI speech API

---
+++ dyslexia-LLM/app/generation/speech.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+TTS_MODEL = "gpt-4o-mini-tts"
+TTS_VOICE = "cedar"
+
+def generate_speech(text: str) -> bytes:
+    
+    response = client.audio.speech.create(
+        model=TTS_MODEL,
+        voice=TTS_VOICE,
+        input=text,
+        instructions=(
+            # basic TTS prompt. To be improved in future
+            "Speak slowly and clearly. Use a warm, supportive tone. "
+            "Pause briefly between sentences and emphasise important words."
+        ),
+        response_format="mp3",
+    )
+    
+    return response.content
